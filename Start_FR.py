8ab5ebca9b696c95c3ed3cf6f9e92c07b4a2125e
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2021-02-11 18:47:55 -0500

    test jpg

--- Start_FR.py
+++ Start_FR.py
@@ -1,14 +1,12 @@
 import os
 import shutil
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter import Tk
 from tkinter.filedialog import askopenfilenames, askdirectory
 import time
-
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 #filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 #print(filename)
-
 
 print("Select the files you want to rename")
 
@@ -31,29 +29,8 @@ new_name = input("Enter the new name for the file\n")
 
 number = 1
 
-'''
-print("Proceed with renaming this file?\nHit enter")
-
-for x in target:
-
-    print("{}   ------->  {}_{}{}".format(x, new_name, number, ext))
-    number += 1
-
-input()
-'''
 f_dest = []
 
-'''
-print(type(new_name))
-
-print(type("_"))
-
-print(type(str(number)))
-
-print(type(ext))
-
-input()
-'''
 for x in target:
     for b in ext:
         string = dest + new_name + "_" + str(number) + str(b)
@@ -61,8 +38,6 @@ for x in target:
     number += 1
 
 number = 1
-
-
 
 for y in range(len(target)):
     shutil.copyfile(target[y], f_dest[y])
