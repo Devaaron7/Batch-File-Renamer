a305924095be6b8f3fb2378d324166d813eaf615
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2021-02-10 19:13:14 -0500

    Copy Feature Working
    Main function works, will start adding option of naming types

--- Start_FR.py
+++ Start_FR.py
@@ -16,7 +16,10 @@ time.sleep(2)
 
 target = list(askopenfilenames())
 
-ext = target[-4:]
+ext = []
+
+for a in target:
+    ext.append(a[-4:])
 
 print("Select where you want to save the renamed file")
 
@@ -28,15 +31,40 @@ new_name = input("Enter the new name for the file\n")
 
 number = 1
 
-
+'''
 print("Proceed with renaming this file?\nHit enter")
 
 for x in target:
 
-    print("{}   ------->  {}_{}{}".format(x, new_name, number,ext))
+    print("{}   ------->  {}_{}{}".format(x, new_name, number, ext))
     number += 1
 
 input()
+'''
+f_dest = []
 
+'''
+print(type(new_name))
+
+print(type("_"))
+
+print(type(str(number)))
+
+print(type(ext))
+
+input()
+'''
 for x in target:
-    shutil.copyfile(x, dest + new_name + ext)
+    for b in ext:
+        string = dest + new_name + "_" + str(number) + str(b)
+    f_dest.append(string)
+    number += 1
+
+number = 1
+
+
+
+for y in range(len(target)):
+    shutil.copyfile(target[y], f_dest[y])
+    number += 1
+
