1d852ab2c4022576fb67cc3451af711726cbdc9c
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2021-02-12 18:41:08 -0500

    Added Comments
    Comments to explain current code. Will add two more features and make code public

--- Mid_FR.py
+++ Mid_FR.py
@@ -16,7 +16,7 @@ target = list(askopenfilenames())
 
 
 
-# For loop that checks every files extension and adds them individually to a empty list (ext)
+# For loop that checks every files extension and adds them individually to a empty list (ext), in the order of selected file
 ext = []
 ct = 1
 for a in target:
@@ -31,8 +31,6 @@ for a in target:
     ext.append(temp[::-1])
     ct = 1
 
-
-
 #Prompts user to select destination folder to copy the renamed files to. Stores the path to the dest variable.
 print("Select where you want to save the renamed file")
 time.sleep(2)
@@ -40,7 +38,8 @@ dest = askdirectory() + "/"
 new_name = input("Enter the new name for the file\n")
 
 
-# For Loop that formats the new name of the file(s) and adds them to a list (f_dest) 
+# For Loop that formats the new name of the file(s) and adds them to a list (f_dest)
+## Format is Currently the new_name + "_" + number starting with 1 + the extension in the list etx
 number = 1
 f_dest = []
 for b in ext:
@@ -49,7 +48,8 @@ for b in ext:
     number += 1
 
 
-
+# For Loop that will copies every selected file from the target list to the destination with every formatted new name..
+## and correct extension
 for y in range(len(target)):
     shutil.copyfile(target[y], f_dest[y])
     number += 1
